e3b9242ca5ae638234a0b094beba316a8e06a51c
ulises <ulisespallares8@gmail.com>
2021-11-09 08:42:49 -0300

    asddd

--- Agenda_contactos/models.py
+++ Agenda_contactos/models.py
@@ -7,8 +7,6 @@ class usuario(models.Model):
 
     def __str__(self):
         cadena = "{0}"
-        print("sdf")
-        print(cadena.format(self.nombre))
         return cadena.format(self.nombre)
 
 class contacto(models.Model):
