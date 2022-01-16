8127449f3d3b1a929a629baedfacbf64736b70c5
ulises <ulisespallares8@gmail.com>
2022-01-15 21:46:48 -0300

    d

--- Agenda/settings.py
+++ Agenda/settings.py
@@ -133,6 +133,9 @@ LOGOUT_REDIRECT_URL = 'inicio'
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
